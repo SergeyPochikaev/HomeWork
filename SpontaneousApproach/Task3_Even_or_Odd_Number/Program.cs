b92c87083f6097ed8abdbcef538f8a3c008395df
Sergey Pochikaev <psvakm@gmail.com>
2023-01-11 21:56:53 +0700

    выполнено задача 3 - индификация числа как четное

---
+++ SpontaneousApproach/Task3_Even_or_Odd_Number/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным.
+
+Console.Clear();
+Console.WriteLine("Условие: Вводите только любое целое число");
+int xa = 0, ya = 1;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("-----------------------------------------------");
+
+Console.Write("Введите значение a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0) Console.WriteLine("Число <<a>> является четным!");
+else
+  Console.WriteLine("Число <<a>> является нечетным!");
