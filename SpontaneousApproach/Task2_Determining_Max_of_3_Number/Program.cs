08c9e2fb164c3946a4f20c857230e6b39c50d6ce
Sergey Pochikaev <psvakm@gmail.com>
2023-01-11 21:50:05 +0700

    выполнено задача 1 - определение max числа из 3 чисел

---
+++ SpontaneousApproach/Task2_Determining_Max_of_3_Number/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача4:  Напишите программу, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+int xa = 0, ya = 1;
+Console.WriteLine("Условие: Водятся только целые числа ");
+Console.WriteLine("-----------------------------------------------");
+
+Console.Write("Введите значение a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b == max)
+{
+  if (c == max) Console.WriteLine("Значения все равны!");
+  else
+  {
+    if (c > max) max = c;
+  Console.Write("max = ");
+  Console.WriteLine(max);
+  }
+}
+else
+{
+  if (b > max) max = b;
+  if (c > max) max = c;
+  Console.Write("max = ");
+  Console.WriteLine(max);
+}
