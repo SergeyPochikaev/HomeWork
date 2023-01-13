d91cd34aed1e31b48406f0072e685874655d07c8
Sergey Pochikaev <psvakm@gmail.com>
2023-01-13 15:14:06 +0700

    Инициализирована задача №1 из блока задач по семинару №2(Базовые алгоритмы)

---
+++ SpontaneousApproach/BasicAlgorithms/Task1_Output_2_Number_from_3digit/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Условие: Вводите только любые целые трехзначные числа");
+int xa = 0, ya = 1;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("-----------------------------------------------");
+
+//int N = Convert.ToInt32(Console.ReadLine());
+int N =0;
+Console.Write($"Введите кол-во случайных чисел N = {N}");
+
