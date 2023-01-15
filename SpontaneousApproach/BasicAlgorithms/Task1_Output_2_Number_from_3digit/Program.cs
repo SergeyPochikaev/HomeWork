34a4abc21e547ea55b42bd3c83716f887d90889c
Sergey Pochikaev <psvakm@gmail.com>
2023-01-15 13:43:41 +0700

    выполнено задача 1 из Семинара 2 по бпзовых алгоритмов

--- SpontaneousApproach/BasicAlgorithms/Task1_Output_2_Number_from_3digit/Program.cs
+++ SpontaneousApproach/BasicAlgorithms/Task1_Output_2_Number_from_3digit/Program.cs
@@ -1,5 +1,5 @@
-﻿//Задача 8: Напишите программу, которая на вход принимает число (N),
-//а на выходе показывает все чётные числа от 1 до N.
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число,
+//и на выходе показывает вторую цифру этого числа..
 
 Console.Clear();
 Console.WriteLine("Условие: Вводите только любые целые трехзначные числа");
@@ -7,7 +7,56 @@ int xa = 0, ya = 1;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("-----------------------------------------------");
 
-//int N = Convert.ToInt32(Console.ReadLine());
-int N =0;
-Console.Write($"Введите кол-во случайных чисел N = {N}");
-
+Console.Write("Введите трехзначное число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if (N < 100)
+{
+  while (N < 100)
+  {
+    Console.Write("Вы ошиблись, читайте внимательно условие!!!\nВедите заново целое трехзначное число N = ");
+    N = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    if (N >= 1000)
+    {
+      while (N >= 1000)
+      {
+        Console.Write("Вы ошиблись, читайте внимательно условие!!!\nВедите заново целое трехзначное число N = ");
+        N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+      }
+    }
+  }
+}
+else
+{
+  if (N >= 1000)
+  {
+    while (N >= 1000)
+    {
+      Console.Write("Вы ошиблись, читайте внимательно условие!!!\nВедите заново целое трехзначное число N = ");
+      N = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine();
+      if (N < 100)
+      {
+        while (N < 100)
+        {
+          Console.Write("Вы ошиблись, читайте внимательно условие!!!\nВедите заново целое трехзначное число N = ");
+          N = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine();
+        }
+      }
+    }
+  }
+}
+int num2 = N % 100;
+if (num2 > 10) num2 = num2 / 10;
+else
+{
+  if (num2 > 0 && num2 < 10)
+  {
+    num2 = num2 / 10;
+    num2 = num2 % 10;
+  }
+}
+Console.Write($"Вторая цифра из числа N = {N} является: {num2}");
