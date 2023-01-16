be205d6944604f8d98a4ef7b03dfae9f1233f48f
Sergey Pochikaev <psvakm@gmail.com>
2023-01-16 14:43:48 +0700

    Выполнено задача №2 из Семинара 2 по базовым алгоритмам

---
+++ SpontaneousApproach/BasicAlgorithms/Task2_Output_3_Number_from_Any_Integer/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+//или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Условие: Вводите только любые целые числа");
+int xa = 0, ya = 1;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("-----------------------------------------");
+
+Console.Write("Ведите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Определение разряда из набраного числа 
+int i = 0;
+int Num = N;
+while (N > 0)
+{
+  N = N / 10;
+  i++;
+}
+// Условия вывода разряда (разряд считается с обратной стороны - ввод цифры)
+Console.Write("Ведите цифру (обратный разряд) от заданного числа Chr = ");
+int j = Convert.ToInt32(Console.ReadLine());
+int chr = j;
+
+if (i == j)
+{
+  chr = Num % 10;
+  Console.WriteLine($"Число N = {Num} имеет {chr} разряд");
+}
+else if (i > j)
+{
+  int degree = Convert.ToInt32(Math.Pow(10, i - j));
+  chr = Num / degree;
+  chr = chr % 10;
+  Console.WriteLine($"Число N = {Num} имеет {chr} разряд");
+}
+else Console.WriteLine($"Число N = {Num} не имеет такой цифры (Разряда)");
