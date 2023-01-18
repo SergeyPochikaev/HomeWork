a5583ea9d71c5cfe2821397bc9737355079079f5
Sergey Pochikaev <psvakm@gmail.com>
2023-01-18 20:35:48 +0700

    Выполнена задачи №1 из Семинара 3

--- ProceduralApproach/Task1_Definition_of_Palindrome_from_5digits/Program.cs
+++ ProceduralApproach/Task1_Definition_of_Palindrome_from_5digits/Program.cs
@@ -1,3 +1,32 @@
-﻿//Задача 15: Напишите программу, которая принимает на вход цифру,
-//обозначающую день недели, и проверяет, является ли этот день выходным.
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число
+//и на выходе проверяет, является ли оно палиндромом.
+
+// Метод - Условие ввода числа
+void InputCondition()
+{
+  Console.Clear();
+  Console.WriteLine("Условие: Введите любое целое число");
+  Console.WriteLine("----------------------------------");
+  Console.Write("Ведите число n = ");
+}
+
+// Метод - Определение палиндрома
+bool CheckOnPalindrome(long value)
+{
+  long n = value;
+  long inverseValue = 0;
+  while (n > 0)
+  {
+    long o = n % 10;
+    inverseValue = inverseValue * 10 + o;
+    n = n / 10;
+  }
+  return value == inverseValue;
+}
+
+InputCondition();
+long n = Convert.ToInt64(Console.ReadLine());
+if (CheckOnPalindrome(n)) Console.WriteLine($"Число n = {n} является палиндромом");
+else Console.WriteLine($"Число n = {n} не является палиндромом");
+
 
