a1f2ac3589cd42164920e2513268c459e52e14e0
Sergey Pochikaev <psvakm@gmail.com>
2023-01-31 19:00:34 +0700

    Выполнена задача №1 из Семинара 7 (Двухмерные массивы)

--- ArrayTwoDimensional/Task1_Matrix_with_Random_Numbers/Program.cs
+++ ArrayTwoDimensional/Task1_Matrix_with_Random_Numbers/Program.cs
@@ -1,21 +1,42 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный
 // случайными вещественными числами.
 
-// Метод задания N элементов для массива
-int GetNumbersByUser(string text)
+// Методом кортежа задается случайный размер матрицы m*n
+(int length, int width) GetSizeMatrix()
 {
-  Console.Clear();
-  Console.Write($"Введите количество элементов в массиве length = {text}");
-  return Convert.ToInt32(Console.ReadLine());
+  int length = Random.Shared.Next(2, 10);
+  int width = Random.Shared.Next(2, 10);
+  return (length, width);
 }
 
-// Метод создания массива случайным образом
-int[] CreateArrayNumbers(int size)
+// Метод создания матрицы случайным образом
+double[,] GetMatrixRandom(int length, int width)
 {
-  int[] array = new int[size];
-  for (int i = 0; i < array.Length; i++)
+  double[,] matrix = new double[length, width];
+  for (int i = 0; i < matrix.GetLength(0); i++)
   {
-    array[i] = new Random().Next(0, 10);
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = Random.Shared.Next(-9, 10) / 1.5;
+    }
   }
-  return array;
+  return matrix;
 }
+
+// Метод распечатки матрицы
+void PrintArray(double[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write($"{Convert.ToDecimal(Math.Round(matr[i, j], 1))};");
+    }
+    Console.WriteLine();
+  }
+}
+
+double[,] number = GetMatrixRandom(GetSizeMatrix().length, GetSizeMatrix().width);
+PrintArray(number);
+
+
