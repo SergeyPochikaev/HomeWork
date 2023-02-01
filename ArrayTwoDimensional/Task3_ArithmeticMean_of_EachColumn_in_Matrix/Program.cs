3c90e1fed8b310df2b37b956df7cb467a85ed3db
Sergey Pochikaev <psvakm@gmail.com>
2023-02-01 19:19:43 +0700

    Выполнено задача №3 из Семинара 7 (Двухмерные массивы)

--- ArrayTwoDimensional/Task3_ArithmeticMean_of_EachColumn_in_Matrix/Program.cs
+++ ArrayTwoDimensional/Task3_ArithmeticMean_of_EachColumn_in_Matrix/Program.cs
@@ -4,8 +4,8 @@
 // Методом кортежа задается случайный размер матрицы m*n
 (int length, int width) GetSizeMatrix()
 {
-  int length = Random.Shared.Next(2, 11);
-  int width = Random.Shared.Next(2, 11);
+  int length = Random.Shared.Next(2, 5);
+  int width = Random.Shared.Next(2, 5);
   return (length, width);
 }
 
@@ -17,8 +17,48 @@ int[,] GetMatrixRandom(int length, int width)
   {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-      matrix[i, j] = Random.Shared.Next(1,10);
+      matrix[i, j] = Random.Shared.Next(1, 10);
     }
   }
   return matrix;
 }
+
+// Метод определения среднеарифметическое значение по каждому столбцу
+double[] GetArithmeticMean(int[,] matrix)
+{
+  double[] arithmeticMean = new double[matrix.GetLength(1)];
+  double sum = 0;
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum = sum + matrix[i, j];
+    }
+    arithmeticMean[j] = Math.Round(sum / matrix.GetLength(0),1);
+  }
+  return arithmeticMean;
+}
+
+// Метод распечатки матрицы
+void PrintArray(int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write($"{matr[i, j]}|");
+    }
+    Console.WriteLine();
+  }
+}
+
+string GoodPrint(double[] arrayInt)
+{
+  return $"{String.Join(";", arrayInt)} - Среднее арифметическое каждого столбца";
+}
+
+int[,] number = GetMatrixRandom(GetSizeMatrix().length, GetSizeMatrix().width);
+double[] arithMean = GetArithmeticMean(number);
+PrintArray(number);
+Console.WriteLine(GoodPrint(arithMean));
