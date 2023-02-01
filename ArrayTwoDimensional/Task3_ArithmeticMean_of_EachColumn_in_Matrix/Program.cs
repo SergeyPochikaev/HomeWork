4b49a4ef8a287dd694c333aea8702e5172401cc6
Sergey Pochikaev <psvakm@gmail.com>
2023-02-01 08:54:56 +0700

    Инициализированно задача №3 из Семинара 7 (Двухмерные массивы)

---
+++ ArrayTwoDimensional/Task3_ArithmeticMean_of_EachColumn_in_Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Методом кортежа задается случайный размер матрицы m*n
+(int length, int width) GetSizeMatrix()
+{
+  int length = Random.Shared.Next(2, 11);
+  int width = Random.Shared.Next(2, 11);
+  return (length, width);
+}
+
+// Метод создания матрицы случайным образом
+int[,] GetMatrixRandom(int length, int width)
+{
+  int[,] matrix = new int[length, width];
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = Random.Shared.Next(1,10);
+    }
+  }
+  return matrix;
+}
