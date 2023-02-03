2f64607bc0b734dc765e05786df921bf1ec0d05a
Sergey Pochikaev <psvakm@gmail.com>
2023-02-03 19:47:39 +0700

    Выполнено задача №1 из Семинара 8 (Двухмерные массивы.Продолжение)

--- ArrayTwoDimensional/Continuation/Task1_FindRow_with_MinSum_of_Element/Program.cs
+++ ArrayTwoDimensional/Continuation/Task1_FindRow_with_MinSum_of_Element/Program.cs
@@ -3,16 +3,16 @@
 // элементов.
 
 // Методом кортежа задается случайный размер матрицы m*m
-int  GetSizeMatrix()
+int GetSizeMatrix()
 {
-  int length = Random.Shared.Next(2, 10);
+  int length = Random.Shared.Next(2, 5);
   return length;
 }
 
 // Метод создания матрицы случайным образом
-double[,] GetMatrixRandom(int length)
+int[,] GetMatrixRandomAndArray(int length)
 {
-  double[,] matrix = new double[length, length];
+  int[,] matrix = new int[length, length];
   for (int i = 0; i < matrix.GetLength(0); i++)
   {
     for (int j = 0; j < matrix.GetLength(1); j++)
@@ -23,18 +23,62 @@ double[,] GetMatrixRandom(int length)
   return matrix;
 }
 
+// Метод определения суммы значение элементов по каждой строке
+int[] GetSumRow(int[,] matrix)
+{
+  int[] sumRow = new int[matrix.GetLength(1)];
+  for (int i = 0; i < matrix.GetLength(1); i++)
+  {
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+      sum = sum + matrix[i, j];
+    }
+    sumRow[i] = sum;
+  }
+  return sumRow;
+}
+
+// Метод определения строки из массива с наименьшей суммой
+void MinSumRow(int[] array)
+{
+  int min = array[0];
+  int rowMin = 0;
+  string res = String.Empty;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] <= min)
+    {
+      min = array[i];
+      rowMin = i;
+      //rowMin = i + 1;
+      //res = res + $"{rowMin} ";
+    }
+  }
+  res = $"{(rowMin + 1),2}";
+  Console.Write(res + " - Номер строки c наименьшим значением");
+}
+
 // Метод распечатки матрицы
-void PrintArray(double[,] matr)
+void PrintArray(int[,] matr)
 {
   for (int i = 0; i < matr.GetLength(0); i++)
   {
     for (int j = 0; j < matr.GetLength(1); j++)
     {
-      Console.Write($"{Math.Round(matr[i, j], 1)}|");
+      Console.Write($"{matr[i, j],2}|");
     }
     Console.WriteLine();
   }
 }
+string GoodPrint(int[] arrayInt)
+{
+  return $"{String.Join(";", arrayInt)} - Сумма каждой строки";
+}
+
 int size = GetSizeMatrix();
-double[,] number = GetMatrixRandom(size);
-PrintArray(number);
+int[,] getMatrix = GetMatrixRandomAndArray(size);
+int[] getSumRow = GetSumRow(getMatrix);
+PrintArray(getMatrix);
+Console.WriteLine(GoodPrint(getSumRow));
+MinSumRow(getSumRow);
