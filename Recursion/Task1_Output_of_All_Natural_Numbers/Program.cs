de96ea29c12cae6deac10a7c1772aea234a72275
Sergey Pochikaev <psvakm@gmail.com>
2023-02-09 20:43:55 +0700

    Выполена задача №1 из Семинара 9 (Рекурсия)

--- Recursion/Task1_Output_of_All_Natural_Numbers/Program.cs
+++ Recursion/Task1_Output_of_All_Natural_Numbers/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information Инициализировано 
-Console.WriteLine("Hello, World!");
+﻿// Задача 56.  Задайте значение N. Напишите программу, которая  
+// выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+// Метод вода 
+int GetValuePoint()
+{
+  Console.Write($"Ведите натуральное значение N = ");
+  int value = Convert.ToInt32(Console.ReadLine());
+  return value;
+}
+
+//Вывод всех натуральных чисел
+string OutputNatural(int n)
+{
+  
+  if (n < 1) return String.Empty;
+  else return $"{n}, " + OutputNatural(n - 1);
+}
+
+int n = GetValuePoint();
+Console.WriteLine($"N = {n} -> " + OutputNatural(n));
